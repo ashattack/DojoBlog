876d813ed7357070878cbbc420fb065226e1424b
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-03-16 10:03:49 -0400

    imported navbar and home components to app and implemented

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'My dojo blog'
+  
   return (
     <div className="App">
+      <Navbar /> 
       <div className="content">
         <h1>{ title }</h1>
+        <Home />
       </div>
     </div>
   );
